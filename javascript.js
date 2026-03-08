033bddb96928430f461c617558c70b09d568d0bc
Ricardo Reyes <HDRickstar@gmail.com>
2026-03-07 19:45:38 -0600

    finished test

--- javascript.js
+++ javascript.js
@@ -1,8 +1,4 @@
-let pi = 3.14
-
-console.log(pi);
-
-const button = document.querySelector("button");
+const button = document.querySelector("#btn");
 
 function greet() {
   const name = prompt("What is your name?");
@@ -11,3 +7,32 @@ function greet() {
 }
 
 button.addEventListener("click", greet);
+
+function lastLetter(string){
+  length = string.length;
+  lastLetter = string.substring(length-1,length);
+  return lastLetter;
+}
+const result = lastLetter("hello");
+document.querySelector("#output").textContent = result;
+
+function fizzBuzz(num){
+  const output = document.querySelector("#fizzbuzz");
+  output.innerHTML = "";
+  counter = 0;
+  while(counter < num){
+    counter++;
+    if(counter % 3 == 0 && counter % 5 == 0){
+      console.log("FizzBuzz"); 
+    }else if(counter% 3 == 0){
+      console.log("Fizz");
+    } else if(counter % 5 == 0){
+      console.log("Buzz");
+    } else{
+      console.log(counter);
+    }
+  }
+}
+
+fizzBuzz(20);
+
